8048f163149820d371d801265a4e1b02d43f5358
notlikelion <notlikelion@gmail.com>
2025-08-05 16:08:45 +0900

    입력부

--- src/step2/ImageGen.java
+++ src/step2/ImageGen.java
@@ -1,4 +1,33 @@
 package step2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class ImageGen {
+    // 실행
+    public static void main(String[] args) {
+        ImageGen gen = new ImageGen();
+        gen.inputData();
+    }
+
+    // 빈 리스트
+    final private List<String> favoriteList = new ArrayList<>();
+
+    final private Scanner scanner = new Scanner(System.in);
+
+    // 입력 받기
+    void inputData() {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("좋아하는 동물을 입력해주세요 : ");
+            String input = scanner.nextLine();
+            // trim() -> space, enter 문자열 앞뒤에 제거
+            if (input.trim().isEmpty()) {
+                System.out.println("제대로 입력해주세요!");
+                i--;
+                continue;
+            }
+            favoriteList.add(input);
+        }
+    }
 }
